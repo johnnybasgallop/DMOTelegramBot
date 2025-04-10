fcc9989cbc53a06437ea39a94be4692001043524
johnnybasgallop <johnnybasgallopwork@outlook.com>
2025-04-10 23:40:02 +0100

    redeploy with new location functionality

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from threading import Thread
 
 # from telegram.helpers import escape_markdown  # if you need it
 import pyshorteners
+import pyshorteners.shorteners
 import stripe
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
@@ -42,6 +43,7 @@ bot = bot_app.bot
 # GLOBAL to store the main event loop used by the bot
 MAIN_LOOP = None
 
+shortener_object = pyshorteners.Shortener()
 # Initialize Flask
 app = Flask(__name__)
 stripe.api_key = STRIPE_API_KEY
@@ -160,9 +162,12 @@ async def send_stripe_link(bot: Bot, user_id: int, monthly: bool):
             client_reference_id=str(user_id),
             subscription_data={'metadata': {'telegram_id': str(user_id)}}
         )
+
+        short_link = shortener_object.tinyurl.short(checkout_session.url)
+
         await bot.send_message(
             user_id,
-            f"Click here to subscribe: {checkout_session.url}"
+                f"Yes bro cmonn, Click here to subscribe 👊🏿 : {short_link}"
         )
         print(f"✅ Sent Stripe subscription link to Telegram ID: {user_id}")
     except Exception as e:
